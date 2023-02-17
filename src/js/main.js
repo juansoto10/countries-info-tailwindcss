77ef7b3673f916465a9c0838432bb24fa2ca1bf6
Juan Soto <jpsoto121@gmail.com>
2023-02-17 18:14:57 -0500

    all countries rendered - home page

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ const countrySection = document.getElementById('country');
 const jsonText = document.getElementById('jsonText');
 
 const api = axios.create({
-  baseURL: 'https://countryapi.io/api/all',
+  baseURL: 'https://countryapi.io/api',
   headers: {
     'Authorization': 'Bearer wzboaEqfDeW6hyr5YAby9NvASkyAqQwLDVvull0N'
   }
@@ -31,183 +31,94 @@ const API_KEY = 'wzboaEqfDeW6hyr5YAby9NvASkyAqQwLDVvull0N'; */
 
 async function getCountries() {
   try {
-    const res = await api.get()
+    const res = await api.get('/all');
     console.log(res);
+
+    const unsortedCountries = res.data;
+
+    const countries = {};
+
+    Object.keys(unsortedCountries).sort().forEach(key => {
+      countries[key] = unsortedCountries[key];
+    })
+
+    const toRender = [];
+
+    for (let key of Object.keys(countries)) {
+      const article = document.createElement('article');
+      article.classList.add('bg-main', 'rounded-md', 'shadow-2xl', 'overflow-hidden', 'dark:bg-gray-800', 'dark:text-sec');
+
+      const a = document.createElement('a');
+      a.href = '/src/country.html';
+
+      const img = document.createElement('img');
+      img.classList.add('h-40', 'lg:h-52', 'w-full', 'object-cover');
+      img.src = `https://flagcdn.com/${key}.svg`;
+
+      const div = document.createElement('div');
+      div.classList.add('px-6', 'py-5');
+
+      const h2 = document.createElement('h2');
+      h2.classList.add('font-bold', 'text-lg');
+      const countryName = document.createTextNode(countries[key]['name']);
+      h2.append(countryName);
+
+      const ul = document.createElement('ul');
+      ul.classList.add('py-3', 'text-sm', 'flex', 'flex-col', 'gap-1');
+
+      const liCapital = document.createElement('li');
+      const capital = countries[key]['capital'];
+      liCapital.innerHTML = `<strong class="font-semibold">Capital:</strong> ${capital}`;
+
+      const liPopulation = document.createElement('li');
+      const population = countries[key]['population'];
+      const formattedPopulation = population.toLocaleString()
+      liPopulation.innerHTML = `<strong class="font-semibold">Population:</strong> ${formattedPopulation}`;
+
+      const liRegion = document.createElement('li');
+      const region = countries[key]['region'];
+      liRegion.innerHTML = `<strong class="font-semibold">Region:</strong> ${region}`;
+
+      ul.append(liCapital, liPopulation, liRegion);
+
+      div.append(h2, ul);
+
+      a.append(img, div);
+
+      article.append(a);
+
+      // Get country
+      /* article.onclick = getCountry(countryName); */
+
+      toRender.push(article);
+    }
+
+    cardsSection.append(...toRender);
   }
   catch (error) {
     console.error(error);
   }
 }
 
-/* getCountries(); */
+
+async function getCountry(name) {
+  try {
+    
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getCountries();
 
 // res.data => {ar:{}, co:{}}
 // countries = res.data
 // 
 // name, population, region, capital
 
-const countries = {
-  "af": {
-    "name": "Afghanistan",
-    "official_name": "Islamic Republic of Afghanistan",
-    "topLevelDomain": [".af"],
-    "alpha2Code": "AF",
-    "alpha3Code": "AFG",
-    "cioc": "AFG",
-    "numericCode": "004",
-    "callingCode": "+93",
-    "capital": "Kabul",
-    "altSpellings": ["AF", "Afġānistān"],
-    "region": "Asia",
-    "subregion": "Southern Asia",
-    "population": 2837743,
-    "latLng": { "country": [33, 65], "capital": [34.52, 69.18] },
-    "demonyms": {
-      "eng": { "f": "Afghan", "m": "Afghan" },
-      "fra": { "f": "Afghane", "m": "Afghan" }
-    },
-    "area": 652230,
-    "gini": "Not Available",
-    "timezones": ["UTC+04:30"],
-    "borders": ["IRN", "PAK", "TKM", "UZB", "TJK", "CHN"],
-    "nativeNames": {
-      "prs": { "official": "جمهوری اسلامی افغانستان", "common": "افغانستان" },
-      "pus": {
-        "official": "د افغانستان اسلامي جمهوریت",
-        "common": "افغانستان"
-      },
-      "tuk": {
-        "official": "Owganystan Yslam Respublikasy",
-        "common": "Owganystan"
-      }
-    },
-    "currencies": { "AFN": { "name": "Afghan afghani", "symbol": "؋" } },
-    "languages": { "prs": "Dari", "pus": "Pashto", "tuk": "Turkmen" },
-    "translations": {
-      "ara": "أفغانستان",
-      "ces": "Afghánistán",
-      "cym": "Affganistan",
-      "deu": "Afghanistan",
-      "est": "Afganistan",
-      "fin": "Afganistan",
-      "fra": "Afghanistan",
-      "hrv": "Afganistan",
-      "hun": "Afganisztán",
-      "ita": "Afghanistan",
-      "jpn": "アフガニスタン",
-      "kor": "아프가니스탄",
-      "nld": "Afghanistan",
-      "per": "افغانستان",
-      "pol": "Afganistan",
-      "por": "Afeganistão",
-      "rus": "Афганистан",
-      "slk": "Afganistan",
-      "spa": "Afganistán",
-      "swe": "Afghanistan",
-      "urd": "افغانستان",
-      "zho": "阿富汗"
-    },
-    "flag": {
-      "small": "https://flagcdn.com/24x18/af.png",
-      "medium": "https://flagcdn.com/96x72/af.png",
-      "large": "https://flagcdn.com/256x192/af.png"
-    },
-    "regionalBlocs": [
-      {
-        "acronym": "SAARC",
-        "name": "South Asian Association for Regional Cooperation"
-      }
-    ]
-  },
-  "sd": {
-    "name": "Sudan",
-    "official_name": "Republic of the Sudan",
-    "topLevelDomain": [".sd"],
-    "alpha2Code": "SD",
-    "alpha3Code": "SDN",
-    "cioc": "SUD",
-    "numericCode": "729",
-    "callingCode": "+249",
-    "capital": "Khartoum",
-    "altSpellings": ["SD", "Republic of the Sudan", "Jumhūrīyat as-Sūdān"],
-    "region": "Africa",
-    "subregion": "Northern Africa",
-    "population": 43849269,
-    "latLng": { "country": [15, 30], "capital": [15.6, 32.53] },
-    "demonyms": {
-      "eng": { "f": "Sudanese", "m": "Sudanese" },
-      "fra": { "f": "Soudanaise", "m": "Soudanais" }
-    },
-    "area": 1886068,
-    "gini": { "2014": 34.2 },
-    "timezones": ["UTC+03:00"],
-    "borders": ["CAF", "TCD", "EGY", "ERI", "ETH", "LBY", "SSD"],
-    "nativeNames": {
-      "ara": { "official": "جمهورية السودان", "common": "السودان" },
-      "eng": { "official": "Republic of the Sudan", "common": "Sudan" }
-    },
-    "currencies": { "SDG": { "name": "Sudanese pound" } },
-    "languages": { "ara": "Arabic", "eng": "English" },
-    "translations": {
-      "ara": "السودان",
-      "ces": "Súdán",
-      "cym": "Sudan",
-      "deu": "Sudan",
-      "est": "Sudaan",
-      "fin": "Sudan",
-      "fra": "Soudan",
-      "hrv": "Sudan",
-      "hun": "Szudán",
-      "ita": "Sudan",
-      "jpn": "スーダン",
-      "kor": "수단",
-      "nld": "Soedan",
-      "per": "سودان",
-      "pol": "Sudan",
-      "por": "Sudão",
-      "rus": "Судан",
-      "slk": "Sudán",
-      "spa": "Sudán",
-      "swe": "Sudan",
-      "urd": "سودان",
-      "zho": "苏丹"
-    },
-    "flag": {
-      "small": "https://flagcdn.com/24x18/sd.png",
-      "medium": "https://flagcdn.com/96x72/sd.png",
-      "large": "https://flagcdn.com/256x192/sd.png"
-    },
-    "regionalBlocs": [
-      {
-        "acronym": "AU",
-        "name": "African Union",
-        "otherNames": [
-          "الاتحاد الأفريقي",
-          "Union africaine",
-          "União Africana",
-          "Unión Africana",
-          "Umoja wa Afrika"
-        ]
-      },
-      {
-        "acronym": "AL",
-        "name": "Arab League",
-        "otherNames": [
-          "جامعة الدول العربية",
-          "Jāmiʻat ad-Duwal al-ʻArabīyah",
-          "League of Arab States"
-        ]
-      }
-    ]
-  },
-}
 
-for (let key of Object.keys(countries)) {
-  console.log(key + " -> " + countries[key]['name']);
-  console.log(key + " -> " + countries[key]['population']);
-  console.log(key + " -> " + countries[key]['region']);
-  console.log(key + " -> " + countries[key]['capital']);
-}
+
+
 
 {/* <article class="pt-16 lg:grid lg:grid-cols-2 lg:gap-10 lg:pt-20 xl:gap-24">
   
@@ -309,33 +220,29 @@ for (let key of Object.keys(countries)) {
 // CARDS
 
 {/* <article class="bg-main rounded-md shadow-2xl overflow-hidden dark:bg-gray-800 dark:text-sec">
-<a href="/src/country.html">
-  <img src="https://flagcdn.com/co.svg" alt="" class="h-40 lg:h-52 w-full object-cover">
+  <a href="/src/country.html">
+    <img src="https://flagcdn.com/co.svg" alt="" class="h-40 lg:h-52 w-full object-cover">
 
-  <div class="px-6 py-5">
-    <h2 class="font-bold text-lg">
-      Colombia
-    </h2>
+    <div class="px-6 py-5">
+      <h2 class="font-bold text-lg">
+        Colombia
+      </h2>
 
-    <ul class="py-3 text-sm flex flex-col gap-1">
-      <li>
-        <strong class="font-semibold">Population:</strong>
-        50,882,884
-      </li>
-      <li>
-        <strong class="font-semibold">Region:</strong> Americas
-      </li>
-      <li>
-        <strong class="font-semibold">Capital:</strong> Bogotá
-      </li>
-    </ul>
-  </div>
-</a>
+      <ul class="py-3 text-sm flex flex-col gap-1">
+        <li>
+          <strong class="font-semibold">Population:</strong>
+          50,882,884
+        </li>
+        <li>
+          <strong class="font-semibold">Region:</strong> Americas
+        </li>
+        <li>
+          <strong class="font-semibold">Capital:</strong> Bogotá
+        </li>
+      </ul>
+    </div>
+  </a>
 </article> */}
-
-
-
-// Cards view
 
 
 // Country view
@@ -345,6 +252,7 @@ for (let key of Object.keys(countries)) {
 
 /* article.classList.add('pt-16', 'lg:grid', 'lg:grid-cols-2', 'lg:gap-10', 'lg:pt-20', 'xl:gap-24'); */
 
+/*
 const article = document.createElement('article');
 article.classList.add('pt-16', 'lg:grid', 'lg:grid-cols-2', 'lg:gap-10', 'lg:pt-20', 'xl:gap-24');
 
@@ -357,4 +265,4 @@ img.alt = '';
 const mainDiv = document.createElement('div');
 mainDiv.classList.add('pt-8', 'xl:flex', 'xl:flex-col', 'xl:justify-between', 'xl:p-0');
 
-
+*/
